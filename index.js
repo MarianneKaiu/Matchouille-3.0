fa177846747c9e82b75391521d3712f335512345
MarianneKaiu <lapierre.marianne@gmail.com>
2023-06-14 09:46:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const glass = document.querySelector(".fa-magnifying-glass");
+const dots = document.querySelectorAll(".fa-solid");
+const more = document.querySelector("#more");
+let cardId;
+
+glass.addEventListener("click", () => {
+    alert(
+        "Je suis désolée, ce bouton n'est pas encore fonctionnel, veuillez retourner sur la page d'accueil"
+    );
+});
+
+dots.forEach((dot) => {
+    dot.addEventListener("click", (e) => {
+        more.classList.toggle("hidden");
+        console.log(e.target.id);
+    });
+});
